407754ee27f3e65ea4b9fabc61f37f705ddf04ac
Bruno Guimarães <bruno.guimaraes@maximatech.com.br>
2024-03-24 17:25:46 -0300

    Alacritty Terminal emulator installation added as well as its config file

---
+++ config.lua
@@ -0,0 +1,50 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+
+-------------------- Options --------------------
+vim.opt.wrap = true
+
+-------------------- Mappings --------------------
+lvim.keys.normal_mode = {
+    ["<M-l>"] = ":bnext<cr>",
+    ["<M-h>"] = ":bprevious<cr>",
+}
+
+lvim.keys.insert_mode["jk"] = "<ESC>"
+
+-------------------- Plugins --------------------
+lvim.colorscheme = "catppuccin"
+
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" } }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", args = { "--ignore=E203" }, filetypes = { "python" } } }
+
+lvim.plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
+}
